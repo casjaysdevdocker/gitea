d8e5a652eefdfd4428992438289afc833def8563
casjay <git-admin@casjaysdev.pro>
2024-08-11 15:26:17 -0400

    🗃️ Committing everything that changed 🗃️
    rootfs/usr/local/etc/docker/init.d/zz-act_runner.sh

--- rootfs/usr/local/etc/docker/init.d/zz-act_runner.sh
+++ rootfs/usr/local/etc/docker/init.d/zz-act_runner.sh
@@ -369,7 +369,7 @@ __post_execute() {
         if [ -n "$multi" ] && [ -d "$multi" ]; then
           unset pid is_running name
           name="$(basename "$multi")"
-          if [ -f "$$multi/$name.yaml" ] && [ -f "$multi/runners" ]; then
+          if [ -f "$multi/$name.yaml" ] && [ -f "$multi/runners" ]; then
             act_runner daemon --config $multi/$name.yaml &
             local pid=$!
             sleep 5 && ps ax | awk '{print $1}' | grep -v grep | grep -q "$pid$" && is_running="yes"
