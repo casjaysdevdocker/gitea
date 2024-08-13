ecb84d7bb6b1a80c4184f3f50b06f9835dc116e6
casjay <git-admin@casjaysdev.pro>
2024-08-13 16:12:20 -0400

    🗃️ Committing everything that changed 🗃️
    rootfs/usr/local/etc/docker/init.d/zz-act_runner.sh

--- rootfs/usr/local/etc/docker/init.d/zz-act_runner.sh
+++ rootfs/usr/local/etc/docker/init.d/zz-act_runner.sh
@@ -253,13 +253,13 @@ EOF
     for runner in "$CONF_DIR/reg"/*.reg; do
       [ -f "$runner" ] && . "$runner"
       while :; do
+        SYS_AUTH_TOKEN="${RUNNER_AUTH_TOKEN:-$(sudo -u gitea gitea --config "$ETC_DIR/gitea/app.ini" actions generate-runner-token 2>/dev/null | grep -v '\.\.\.')}"
         RUNNER_NAME="${RUNNER_NAME:-$(basename "${runner//.reg/}")}"
         RUNNER_HOME="${RUNNER_HOME:-$CONF_DIR/multi/$RUNNER_NAME}"
-        RUNNER_HOSTNAME="https://${RUNNER_HOSTNAME:-$HOSTNAME}"
-        RUNNER_REGISTER_URL="${RUNNER_REGISTER_URL:-127.0.0.1:8000}"
+        RUNNER_HOSTNAME="${RUNNER_HOSTNAME:-http://$HOSTNAME}"
+        RUNNER_REGISTER_URL="${RUNNER_REGISTER_URL:-http://127.0.0.1:8000}"
         RUNNER_AUTH_TOKEN="${RUNNER_AUTH_TOKEN:-$SYS_AUTH_TOKEN}"
         RUNNER_LABELS="${RUNNER_LABELS:-act_runner}"
-        SYS_AUTH_TOKEN="${RUNNER_AUTH_TOKEN:-$(sudo -u gitea gitea --config "$ETC_DIR/gitea/app.ini" actions generate-runner-token 2>/dev/null | grep -v '\.\.\.')}"
         if [ ! -f "$RUNNER_HOME/runners" ]; then
           [ -n "$RUNNER_NAME" ] && [ -n "$RUNNER_HOME" ] || break
           [ -f "$CONF_DIR/tokens/$RUNNER_NAME" ] && RUNNER_AUTH_TOKEN="$(<"$CONF_DIR/tokens/$RUNNER_NAME")" || { [ -n "$SYS_AUTH_TOKEN" ] && echo "$SYS_AUTH_TOKEN" >"$CONF_DIR/tokens/$RUNNER_NAME"; }
